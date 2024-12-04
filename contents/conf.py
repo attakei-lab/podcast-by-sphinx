8de22ec766692e863e776bd627114347300ad044
Kazuya Takei <myself@attakei.net>
2024-12-04 12:38:44 +0900

    feat: Set extensions

--- contents/conf.py
+++ contents/conf.py
@@ -6,23 +6,26 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Demo'
-copyright = '2024, Kazuya Takei'
-author = 'Kazuya Takei'
-release = '0'
+project = "Demo"
+copyright = "2024, Kazuya Takei"
+author = "Kazuya Takei"
+release = "0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "atsphinx.audioplayer",
+    "rst_package_refs.sphinx",
+]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
-language = 'ja'
+language = "ja"
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
--- contents/index.rst
+++ contents/index.rst
@@ -1,8 +1,3 @@
-.. Demo documentation master file, created by
-   sphinx-quickstart on Sun Dec  1 01:37:49 2024.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
-
 Demo documentation
 ==================
 
@@ -15,3 +10,4 @@ documentation for details.
    :maxdepth: 2
    :caption: Contents:
 
+   episodes/index
